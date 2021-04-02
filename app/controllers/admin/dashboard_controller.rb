75a7db258370f584d4d014ad0bcc2cc935cd6375
SPARTAN-501 <luke.yin@alumni.ubc.ca>
2021-04-02 00:22:56 +0000

    Finished feature/dashboard-counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["HTTP_BASIC_AUTH_USER"], password: ENV["HTTP_BASIC_AUTH_PASS"]
 
   def show
+    @productcount = Product.count
+    @categorycount = Category.count
   end
 end
