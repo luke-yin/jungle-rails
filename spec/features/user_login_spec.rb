eac5cafcfb6756e827d3d91a26951c3157f81f91
SPARTAN-501 <luke.yin@alumni.ubc.ca>
2021-04-07 03:30:15 +0000

    Finished testing user login

---
+++ spec/features/user_login_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.feature "UserLogins", type: :feature, js: true do
+  before :each do
+    @user = User.create(name: "Test", email: "test@test.com", password: "password", password_confirmation: "password")
+  end
+
+  scenario "User is able to login" do
+    # ACT
+    visit '/login'
+    fill_in 'email', with: 'test@test.com'
+    fill_in 'password', with: 'password'
+    click_button 'Submit'
+
+    # DEBUG
+    # save_and_open_screenshot
+
+    # VERIFY
+    expect(page).to have_link 'Logout', href: '/logout'
+  end
+end
